f341914c856c89481877f56cca0d1aa5c214f20f
chw9 <43727884+chw9@users.noreply.github.com>
2019-12-06 22:48:32 -0500

    submodule

--- pathfinder/AbstractDrive.java
+++ pathfinder/AbstractDrive.java
@@ -1,14 +1,11 @@
 package org.frc5587.lib.pathfinder;
 
-import com.ctre.phoenix.motion.MotionProfileStatus;
-import com.ctre.phoenix.motion.SetValueMotionProfile;
-import com.ctre.phoenix.motion.TrajectoryPoint;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANAnalog;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 import org.frc5587.lib.TitanDrive;
 import org.frc5587.lib.TitanDrive.DriveSignal;
@@ -19,46 +16,52 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public abstract class AbstractDrive extends Subsystem {
-    protected TalonSRX leftMaster, rightMaster;
-    protected IMotorController leftSlave, rightSlave;
+    protected CANSparkMax leftLeader, rightLeader;
+    protected CANSparkMax leftFollower, rightFollower;
+    protected CANPIDController spark_pidControllerLeft, spark_pidControllerRight;
+    protected CANEncoder leftSparkEncoder, rightSparkEncoder;
+    protected CANAnalog leftSparkAnalog, rightSparkAnalog;
     protected GyroBase gyro;
     protected AHRS ahrs;
 
-    private double maxVelocity = 2500; // Max velocity in STU
+    int minBufferCount;
+
+    // convert to whatever unit sparks are in
+    private double maxVelocity = 2500; // Max velocity in STU (max value is 4096)
     private int timeoutMS = 10;
-    public int stuPerRev, stuPerInch, minBufferCount;
+    // public int stuPerRev, stuPerInch, minBufferCount;
     public double wheelDiameterMeters;
 
-    private MotionProfileStatus[] statuses = { new MotionProfileStatus(), new MotionProfileStatus() };
+    // private MotionProfileStatus[] statuses = { new MotionProfileStatus(), new MotionProfileStatus() };
     public Notifier profileNotifer = new Notifier(new ProcessProfileRunnable());
 
-    public AbstractDrive(TalonSRX leftMaster, TalonSRX rightMaster, IMotorController leftSlave,
-            IMotorController rightSlave, boolean flipRight) {
+    public AbstractDrive(CANSparkMax leftLeader, CANSparkMax rightLeader, CANSparkMax leftFollower,
+            CANSparkMax rightFollower, boolean flipRight) {
         // Set motors to correct objects and connect the masters and slaves
-        this.leftMaster = leftMaster;
-        this.leftSlave = leftSlave;
-        this.rightMaster = rightMaster;
-        this.rightSlave = rightSlave;
-        leftSlave.follow(leftMaster);
-        rightSlave.follow(rightMaster);
+        this.leftLeader = leftLeader;
+        this.leftFollower = leftFollower;
+        this.rightLeader = rightLeader;
+        this.rightFollower = rightFollower;
+        leftFollower.follow(leftLeader);
+        rightFollower.follow(rightLeader);
 
         // Left should not be reversed
-        leftMaster.setInverted(false);
-        leftSlave.setInverted(false);
+        leftLeader.setInverted(false);
+        leftFollower.setInverted(false);
 
         // Invert the right side of the drivetrain
         if (flipRight) {
-            rightMaster.setInverted(true);
-            rightSlave.setInverted(true);
+            rightLeader.setInverted(true);
+            rightFollower.setInverted(true);
         } else {
             // The Talon might have been used in reversed state in past, so just to be sure
-            rightMaster.setInverted(false);
-            rightSlave.setInverted(false);
+            rightLeader.setInverted(false);
+            rightFollower.setInverted(false);
         }
 
         // Set encoder rotation as opposite direction relative to motor rotation
-        this.leftMaster.setSensorPhase(true);
-        this.rightMaster.setSensorPhase(true);
+        // this.leftLeader.setSensorPhase(true);
+        // this.rightMaster.setSensorPhase(true);
 
         this.ahrs = null;
         this.gyro = null;
@@ -76,12 +79,12 @@ public abstract class AbstractDrive extends Subsystem {
         this.ahrs = ahrs;
     }
 
-    public void setConstants(double maxVelocity, int timeoutMS, int stuPerRev, int stuPerInch,
+    public void setConstants(double maxVelocity, int timeoutMS, /*int stuPerRev, int stuPerInch,*/
             double wheelDiameterMeters, int minBufferCount) {
         this.maxVelocity = maxVelocity;
         this.timeoutMS = timeoutMS;
-        this.stuPerRev = stuPerRev;
-        this.stuPerInch = stuPerInch;
+        // this.stuPerRev = stuPerRev;
+        // this.stuPerInch = stuPerInch;
         this.wheelDiameterMeters = wheelDiameterMeters;
         this.minBufferCount = minBufferCount;
     }
@@ -92,15 +95,15 @@ public abstract class AbstractDrive extends Subsystem {
 
     public void enableBrakeMode(boolean enabled) {
         if (enabled) {
-            leftMaster.setNeutralMode(NeutralMode.Brake);
-            rightMaster.setNeutralMode(NeutralMode.Brake);
-            leftSlave.setNeutralMode(NeutralMode.Brake);
-            rightSlave.setNeutralMode(NeutralMode.Brake);
+            leftLeader.setIdleMode(IdleMode.kBrake);
+            rightLeader.setIdleMode(IdleMode.kBrake);
+            leftFollower.setIdleMode(IdleMode.kBrake);
+            rightFollower.setIdleMode(IdleMode.kBrake);
         } else {
-            leftMaster.setNeutralMode(NeutralMode.Coast);
-            rightMaster.setNeutralMode(NeutralMode.Coast);
-            leftSlave.setNeutralMode(NeutralMode.Coast);
-            rightSlave.setNeutralMode(NeutralMode.Coast);
+            leftLeader.setIdleMode(IdleMode.kCoast);
+            rightLeader.setIdleMode(IdleMode.kCoast);
+            leftFollower.setIdleMode(IdleMode.kCoast);
+            rightFollower.setIdleMode(IdleMode.kCoast);
         }
     }
 
@@ -109,102 +112,107 @@ public abstract class AbstractDrive extends Subsystem {
     public void vbusCurve(double throttle, double curve, boolean isQuickTurn) {
         DriveSignal d = TitanDrive.curvatureDrive(throttle, curve, isQuickTurn);
 
-        leftMaster.set(ControlMode.PercentOutput, d.left);
-        rightMaster.set(ControlMode.PercentOutput, d.right);
+        leftLeader.set(d.left);
+        rightLeader.set(d.right);
     }
 
     public void vbusArcade(double throttle, double turn) {
         DriveSignal d = TitanDrive.arcadeDrive(throttle, turn);
 
-        leftMaster.set(ControlMode.PercentOutput, d.left);
-        rightMaster.set(ControlMode.PercentOutput, d.right);
+        leftLeader.set(d.left);
+        rightLeader.set(d.right);
     }
 
     public void vbusLR(double left, double right) {
-        leftMaster.set(ControlMode.PercentOutput, left);
-        rightMaster.set(ControlMode.PercentOutput, right);
+        leftLeader.set(left);
+        rightLeader.set(right);
     }
 
     public void velocityCurve(double throttle, double curve, boolean isQuickTurn) {
         DriveSignal d = TitanDrive.curvatureDrive(throttle, curve, isQuickTurn);
 
-        leftMaster.set(ControlMode.Velocity, d.left * maxVelocity);
-        rightMaster.set(ControlMode.Velocity, d.right * maxVelocity);
+        // convert velocity to percent output?
+        // leftMaster.set(ControlMode.Velocity, d.left * maxVelocity);
+        // rightMaster.set(ControlMode.Velocity, d.right * maxVelocity);
     }
 
     public void velocityArcade(double throttle, double turn) {
         DriveSignal d = TitanDrive.arcadeDrive(throttle, turn);
 
-        leftMaster.set(ControlMode.Velocity, d.left * maxVelocity);
-        rightMaster.set(ControlMode.Velocity, d.right * maxVelocity);
+        // leftMaster.set(ControlMode.Velocity, d.left * maxVelocity);
+        // rightMaster.set(ControlMode.Velocity, d.right * maxVelocity);
     }
 
     public void stop() {
-        leftMaster.neutralOutput();
-        rightMaster.neutralOutput();
+        leftLeader.disable();
+        rightLeader.disable();
     }
 
     /* --- MOTION PROFILE HANDLING CODE --- */
 
     public class ProcessProfileRunnable implements java.lang.Runnable {
         public void run() {
-            leftMaster.processMotionProfileBuffer();
-            rightMaster.processMotionProfileBuffer();
+            // leftLeader.processMotionProfileBuffer();
+            // rightLeader.processMotionProfileBuffer();
         }
     }
 
     public void resetMP() {
-        leftMaster.clearMotionProfileHasUnderrun(timeoutMS);
-        leftMaster.clearMotionProfileTrajectories();
-        leftMaster.changeMotionControlFramePeriod(10);
-        leftMaster.configMotionProfileTrajectoryPeriod(10, timeoutMS);
-        leftMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, timeoutMS);
-        leftMaster.set(ControlMode.MotionProfile, SetValueMotionProfile.Disable.value);
+    //     leftMaster.clearMotionProfileHasUnderrun(timeoutMS);
+    //     leftMaster.clearMotionProfileTrajectories();
+    //     leftMaster.changeMotionControlFramePeriod(10);
+    //     leftMaster.configMotionProfileTrajectoryPeriod(10, timeoutMS);
+    //     leftMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, timeoutMS);
+    //     leftMaster.set(ControlMode.MotionProfile, SetValueMotionProfile.Disable.value);
 
-        rightMaster.clearMotionProfileHasUnderrun(timeoutMS);
-        rightMaster.clearMotionProfileTrajectories();
-        rightMaster.changeMotionControlFramePeriod(10);
-        rightMaster.configMotionProfileTrajectoryPeriod(10, timeoutMS);
-        rightMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, timeoutMS);
-        rightMaster.set(ControlMode.MotionProfile, SetValueMotionProfile.Disable.value);
+    //     rightMaster.clearMotionProfileHasUnderrun(timeoutMS);
+    //     rightMaster.clearMotionProfileTrajectories();
+    //     rightMaster.changeMotionControlFramePeriod(10);
+    //     rightMaster.configMotionProfileTrajectoryPeriod(10, timeoutMS);
+    //     rightMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, timeoutMS);
+    //     rightMaster.set(ControlMode.MotionProfile, SetValueMotionProfile.Disable.value);
     }
 
     public void updateStatus() {
-        leftMaster.getMotionProfileStatus(statuses[0]);
-        rightMaster.getMotionProfileStatus(statuses[1]);
+        // leftMaster.getMotionProfileStatus(statuses[0]);
+        // rightMaster.getMotionProfileStatus(statuses[1]);
     }
 
-    public boolean isMPReady() {
-        boolean leftReady = getStatuses()[0].btmBufferCnt > minBufferCount;
-        boolean rightReady = getStatuses()[0].btmBufferCnt > minBufferCount;
-        return leftReady && rightReady;
-    }
+    // public boolean isMPReady() {
+    //     // boolean leftReady = getStatuses()[0].btmBufferCnt > minBufferCount;
+    //     // boolean rightReady = getStatuses()[0].btmBufferCnt > minBufferCount;
+    //     return leftReady && rightReady;
+    // }
 
-    public boolean isMPDone() {
-        boolean leftDone = getStatuses()[0].isLast;
-        boolean rightDone = getStatuses()[0].isLast;
-        return leftDone && rightDone;
-    }
+    // public boolean isMPDone() {
+    //     boolean leftDone = getStatuses()[0].isLast;
+    //     boolean rightDone = getStatuses()[0].isLast;
+    //     return leftDone && rightDone;
+    // }
 
-    public void queuePoints(TrajectoryPoint[][] trajectories) {
-        for (TrajectoryPoint point : trajectories[0]) {
-            leftMaster.pushMotionProfileTrajectory(point);
-        }
-        for (TrajectoryPoint point : trajectories[1]) {
-            rightMaster.pushMotionProfileTrajectory(point);
-        }
-    }
+    // public void queuePoints(TrajectoryPoint[][] trajectories) {
+        // for (TrajectoryPoint point : trajectories[0]) {
+        //     leftMaster.pushMotionProfileTrajectory(point);
+        // }
+        // for (TrajectoryPoint point : trajectories[1]) {
+        //     rightMaster.pushMotionProfileTrajectory(point);
+        // }
+    // }
 
-    public void setProfileMode(SetValueMotionProfile mpMode) {
-        leftMaster.set(ControlMode.MotionProfile, mpMode.value);
-        rightMaster.set(ControlMode.MotionProfile, mpMode.value);
-    }
+    // public void setProfileMode(SetValueMotionProfile mpMode) {
+        // leftMaster.set(ControlMode.MotionProfile, mpMode.value);
+        // rightMaster.set(ControlMode.MotionProfile, mpMode.value);
+    // }
 
     /* --- UTILITY METHODS --- */
 
     public void resetEncoders() {
-        leftMaster.setSelectedSensorPosition(0, 0, timeoutMS);
-        rightMaster.setSelectedSensorPosition(0, 0, timeoutMS);
+        leftSparkEncoder.setPosition(0);
+        rightSparkEncoder.setPosition(0);
+
+        // ???
+        leftSparkEncoder.setMeasurementPeriod(timeoutMS);
+        rightSparkEncoder.setMeasurementPeriod(timeoutMS);
     }
 
     public void sendDebugInfo() {
@@ -216,36 +224,49 @@ public abstract class AbstractDrive extends Subsystem {
     }
 
     public void sendMPDebugInfo() {
-        SmartDashboard.putNumber("Left Expected Pos", leftMaster.getActiveTrajectoryPosition());
-        SmartDashboard.putNumber("Right Expected Pos", rightMaster.getActiveTrajectoryPosition());
-        SmartDashboard.putNumber("Left Expected Vel", leftMaster.getActiveTrajectoryVelocity());
-        SmartDashboard.putNumber("Right Expected Vel", rightMaster.getActiveTrajectoryVelocity());
+        SmartDashboard.putNumber("Left Pos", leftSparkEncoder.getPosition());
+        SmartDashboard.putNumber("Right Pos", rightSparkEncoder.getPosition());
+        SmartDashboard.putNumber("Left Vel", leftSparkEncoder.getVelocity());
+        SmartDashboard.putNumber("Right Vel", rightSparkEncoder.getVelocity());
+        // SmartDashboard.putNumber("Left Expected Pos", leftLeader.getActiveTrajectoryPosition());
+        // SmartDashboard.putNumber("Right Expected Pos", rightMaster.getActiveTrajectoryPosition());
+        // SmartDashboard.putNumber("Left Expected Vel", leftMaster.getActiveTrajectoryVelocity());
+        // SmartDashboard.putNumber("Right Expected Vel", rightMaster.getActiveTrajectoryVelocity());
     }
 
     /* --- GETTER METHODS --- */
 
     public int getLeftPosition() {
-        return leftMaster.getSelectedSensorPosition(0);
+        // returns in volts
+        return (int)leftSparkEncoder.getPosition();
     }
 
     public int getRightPosition() {
-        return rightMaster.getSelectedSensorPosition(0);
+        // return rightMaster.getSelectedSensorPosition(0);
+        // returns in volts
+        return (int)rightSparkEncoder.getPosition();
+
     }
 
     public int getLeftVelocity() {
-        return leftMaster.getSelectedSensorVelocity(0);
+        // return leftMaster.getSelectedSensorVelocity(0);
+        // returns in volts per second
+        return (int)leftSparkEncoder.getVelocity();
     }
 
     public int getRightVelocity() {
-        return rightMaster.getSelectedSensorVelocity(0);
+        // returns in volts per second
+        return (int)rightSparkEncoder.getVelocity();
     }
 
     public double getLeftVoltage() {
-        return leftMaster.getMotorOutputVoltage();
+        return leftSparkAnalog.getVoltage();
+        // return leftMaster.getMotorOutputVoltage();
     }
 
     public double getRightVoltage() {
-        return rightMaster.getMotorOutputVoltage();
+        return rightSparkAnalog.getVoltage();
+        // return rightMaster.getMotorOutputVoltage();
     }
 
     public double getHeading() {
@@ -265,7 +286,7 @@ public abstract class AbstractDrive extends Subsystem {
         return ((heading > 180.0) ? (heading - 360.0) : ((heading < -180.0) ? (heading + 360.0) : heading));
     }
 
-    public MotionProfileStatus[] getStatuses() {
-        return statuses;
-    }
+    // public MotionProfileStatus[] getStatuses() {
+    //     return statuses;
+    // }
 }
--- pathfinder/MotionProfileRunner.java
+++ pathfinder/MotionProfileRunner.java
@@ -1,44 +1,44 @@
-package org.frc5587.lib.pathfinder;
+// package org.frc5587.lib.pathfinder;
 
-import com.ctre.phoenix.motion.SetValueMotionProfile;
+// import com.ctre.phoenix.motion.SetValueMotionProfile;
 
-import edu.wpi.first.wpilibj.command.Command;
+// import edu.wpi.first.wpilibj.command.Command;
 
-public class MotionProfileRunner extends Command {
-    AbstractDrive drive;
-    boolean ready = false;
+// public class MotionProfileRunner extends Command {
+//     AbstractDrive drive;
+//     boolean ready = false;
 
-    public MotionProfileRunner(AbstractDrive drivetrain) {
-        this.drive = drivetrain;
-        requires(drivetrain);
-    }
+//     public MotionProfileRunner(AbstractDrive drivetrain) {
+//         this.drive = drivetrain;
+//         requires(drivetrain);
+//     }
 
-    public void initialize() {
-        drive.setProfileMode(SetValueMotionProfile.Disable);
-        drive.enableBrakeMode(true);
-    }
+//     public void initialize() {
+//         drive.setProfileMode(SetValueMotionProfile.Disable);
+//         drive.enableBrakeMode(true);
+//     }
 
-    public void execute() {
-        if (!ready) {
-            ready = drive.isMPReady();
-        } else {
-            drive.setProfileMode(SetValueMotionProfile.Enable);
-        }
-        drive.sendDebugInfo();
-        drive.sendMPDebugInfo();
-    }
+//     public void execute() {
+//         if (!ready) {
+//             ready = drive.isMPReady();
+//         } else {
+//             drive.setProfileMode(SetValueMotionProfile.Enable);
+//         }
+//         drive.sendDebugInfo();
+//         drive.sendMPDebugInfo();
+//     }
 
-    public boolean isFinished() {
-        return drive.isMPDone();
-    }
+//     public boolean isFinished() {
+//         return drive.isMPDone();
+//     }
 
-    public void end() {
-        drive.resetMP();
-        drive.stop();
-        drive.enableBrakeMode(false);
-    }
+//     public void end() {
+//         drive.resetMP();
+//         drive.stop();
+//         drive.enableBrakeMode(false);
+//     }
 
-    public void interrupted() {
-        end();
-    }
-}
+//     public void interrupted() {
+//         end();
+//     }
+// }
